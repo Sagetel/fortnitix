99e4b583dbd29eb27b8a28934cf11b94d2947894
MadoGold <82536105+MadoGold@users.noreply.github.com>
2024-12-04 23:39:50 +0300

    add components Search, CardsWrapper, ProductCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
+import CardsWrapper from './components/CardsWrapper';
+import Search from './components/Search';
+
 import './App.css';
+
+import { Box } from '@mui/material';
 
 function App() {
   return (
     <div>
       <Header />
+      <Box sx={{ p: 10 }}>
+        <Search />
+        <CardsWrapper />
+      </Box>
     </div>
   );
 }
