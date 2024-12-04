d27827d50afb5c52ff82462d988075f089edd068
Sagetel <102986847+Sagetel@users.noreply.github.com>
2024-12-04 23:43:41 +0300

    Authorization

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import AuthorizationMenu from "./AuthorizationMenu";
 
 export default function Header() {
+  const [isOpenPopup, setIsOpenPopup] = useState(false);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -13,7 +16,8 @@ export default function Header() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Fortnite Skins
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" onClick={()=>{setIsOpenPopup(true)}}>Log in</Button>
+          <AuthorizationMenu isOpenPopup={isOpenPopup} setIsOpenPopup={setIsOpenPopup}/>
         </Toolbar>
       </AppBar>
     </Box>
